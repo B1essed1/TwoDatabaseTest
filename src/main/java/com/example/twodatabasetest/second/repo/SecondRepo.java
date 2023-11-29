128b611ec8c80c7e3232bc9e29eab8782d3aedd8
Abdulla Axmadaliyev <aa1549aa@gmail.com>
2023-11-29 16:46:23 +0500

    fix

--- src/main/java/com/example/twodatabasetest/second/repo/SecondRepo.java
+++ src/main/java/com/example/twodatabasetest/second/repo/SecondRepo.java
@@ -8,11 +8,11 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@Transactional(value = "t2", propagation = Propagation.SUPPORTS)
-//@Transactional2(propagation = Propagation.SUPPORTS)
+//@Transactional(value = "t2", propagation = Propagation.SUPPORTS)
+@Transactional2(propagation = Propagation.SUPPORTS)
 public interface SecondRepo extends JpaRepository<SecondEntity, Long> {
     @Override
-    @Transactional(value = "t2")
-//    @Transactional2
+//    @Transactional(value = "t2")
+    @Transactional2
     <S extends SecondEntity> S save(S entity);
 }
